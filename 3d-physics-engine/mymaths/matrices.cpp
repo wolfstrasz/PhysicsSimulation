9688be0a6ea22acae4c10f14b45747e5f65a5df1
Boyan Yotov <boyan.yotov@abv.bg>
2020-02-04 14:41:47 +0000

    Added perspective and orthographic projections.

--- 3d-physics-engine/mymaths/matrices.cpp
+++ 3d-physics-engine/mymaths/matrices.cpp
@@ -382,3 +382,31 @@ mat4 LookAt(const vec3& position, const vec3& target, const vec3& up) {
       right.z, newUp.z, forward.z, 0.0f, -Dot(right, position),
       -Dot(newUp, position), -Dot(forward, position), 1.0f);
 }
+
+mat4 Projection(float fov, float aspect, float zNear, float zFar) {
+  float tanHalfFov = tanf(DEG2RAD((fov * 0.5f)));  // tan(fov/2)
+  float fovY = 1.0f / tanHalfFov;                  // cot(fov/2)
+  float fovX = fovY / aspect;                      // cot(fov/2) / aspect
+  mat4 result;
+  result._11 = fovX;
+  result._22 = fovY;
+  // _33 = far / range
+  result._33 = zFar / (zFar - zNear);
+  result._34 = 1.0f;
+  // _43 = - near * (far / range)
+  result._43 = -zNear * result._33;
+  result._44 = 0.0f;
+  return result;
+}
+
+mat4 Ortho(float left, float right, float bottom, float top, float zNear,
+           float zFar) {
+  float _11 = 2.0f / (right - left);
+  float _22 = 2.0f / (top - bottom);
+  float _33 = 1.0f / (zFar - zNear);
+  float _41 = (left + right) / (left - right);
+  float _42 = (top + bottom) / (bottom - top);
+  float _43 = (zNear) / (zNear - zFar);
+  return mat4(_11, 0.0f, 0.0f, 0.0f, 0.0f, _22, 0.0f, 0.0f, 0.0f, 0.0f, _33,
+              0.0f, _41, _42, _43, 1.0f);
+}
--- 3d-physics-engine/mymaths/matrices.h
+++ 3d-physics-engine/mymaths/matrices.h
@@ -170,4 +170,8 @@ mat4 Transform(const vec3& scale, const vec3& rotationAxis, float rotationAngle,
 // VIEW TRANSFORM
 mat4 LookAt(const vec3& position, const vec3& target, const vec3& up);
 
+// PROJECTION TRANSFORM
+mat4 Projection(float fov, float aspect, float zNear, float zFar);
+mat4 Ortho(float left, float right, float bottom, float top, float zNear,
+           float zFar);
 #endif  // !_H_MATH_MATRICES
